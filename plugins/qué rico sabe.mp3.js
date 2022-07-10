298cb5c522e677623c5d7a2e1b8b552f47ab007b
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-07-10 18:29:39 -0500

    Add files via upload

---
+++ plugins/qué rico sabe.mp3.js
@@ -0,0 +1,14 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = './media/qué rico sabe.mp3'
+conn.sendFile(m.chat, vn, 'qué rico sabe.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /que rico|Que rico/
+handler.command = new RegExp
+export default handler
