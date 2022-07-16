731500261caa81ad92853eb671d84dbf335714a7
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-07-16 01:59:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ const { state, saveState } = useSingleFileAuthState(global.authFile)
 const connectionOptions = {
 printQRInTerminal: true,
 auth: state,
-browser: ['Thehades-Bot','Edge','1.0.0'],
+browser: ['The-hades-Bot','Edge','1.0.0'],
 }
 
 global.conn = makeWASocket(connectionOptions)
