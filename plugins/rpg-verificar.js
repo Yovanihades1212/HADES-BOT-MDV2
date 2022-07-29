fb3f91e5ede57367ce083514774d8f652cd8ce7e
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-07-29 11:51:00 -0500

    Add files via upload

---
+++ plugins/rpg-verificar.js
@@ -0,0 +1,29 @@
+import { createHash } from 'crypto'
+let handler = async function (m, { text, usedPrefix }) {
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+let user = global.db.data.users[m.sender]
+if (user.registered === true) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙷𝙴𝚈! 𝚈𝙰 𝙴𝚂𝚃𝙰𝚂 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾*\n\n*𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝚀𝚄𝙸𝚃𝙰𝚁 𝚃𝚄 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾? 𝚄𝚂𝙰 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}unreg <numero de serie>*\n\n*𝚂𝙸 𝙽𝙾 𝚁𝙴𝙲𝚄𝙴𝚁𝙳𝙰𝚂 𝚃𝚄 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴 𝙿𝚄𝙴𝙳𝙴𝚂 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}myns*`
+let name = conn.getName(m.sender)
+let age = Math.floor(Math.random() * 41)
+age = parseInt(age)
+user.name = name.trim()
+user.age = age
+user.regTime = + new Date
+user.registered = true
+let sn = createHash('md5').update(m.sender).digest('hex')
+let caption = `
+┌──「𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎𝐍」─
+├❏𝙽𝙾𝙼𝙱𝚁𝙴: ${name}
+├❏𝙴𝙳𝙰𝙳: ${age} años
+├❏𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:
+├❏${sn}
+└────ׂ─ׂ─ׂ─ׂ───`
+let author = global.author
+conn.sendButton(m.chat, caption, `¡𝚃𝚄 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴 𝚃𝙴 𝚂𝙴𝚁𝚅𝙸𝚁𝙰 𝙿𝙾𝚁 𝚂𝙸 𝙳𝙴𝚂𝙴𝙰𝚂 𝙱𝙾𝚁𝚁𝙰𝚁 𝚃𝚄 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾 𝙴𝙽 𝙴𝙻 𝙱𝙾𝚃!\n${author}`, [['¡¡𝙰𝙷𝙾𝚁𝙰 𝚂𝙾𝚈 𝚄𝙽 𝚅𝙴𝚁𝙸𝙵𝙸𝙲𝙰𝙳𝙾/𝙰!!', '/profile']], m)
+global.db.data.users[m.sender].money += 10000
+global.db.data.users[m.sender].exp += 10000
+}
+handler.help = ['verificar']
+handler.tags = ['xp']
+handler.command = /^(verify|register|verificar)$/i
+export default handler
