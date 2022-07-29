e7e8ed4c06f9d7012e66d1efac9a26607152a69b
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-07-29 11:36:27 -0500

    Delete plugins directory

--- plugins/Frases▪Test.js
+++
@@ -1,8 +0,0 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn }) => {
-let res = await fetch("https://api.zacros.my.id/randomtext/fml")
-let json = await res.json()
-let { result } = json
-conn.reply(m.chat, `*┏━━━━━━━━━━━━━━━━┓*\n*┠❧  ${result}*\n*┗━━━━━━━━━━━━━━━━┚*`, m)}
-handler.command = /^(frasetest)$/i
-export default handler
