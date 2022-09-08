874e7871af3b4f01d323ff9a6773581d81aade07
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-09-07 22:45:31 -0500

    Add files via upload

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -58,9 +58,9 @@ rows: [
     {title: "│🗝️│𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝚁 𝙴𝙽 𝙴𝙻 𝙶𝚁𝚄𝙿𝙾", description: "𝙴𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚎𝚜 𝚙𝚊𝚛𝚊 𝚊𝚍𝚖𝚒𝚗𝚒𝚜𝚝𝚛𝚊𝚍𝚘𝚛𝚎𝚜 𝚍𝚎𝚕 𝚐𝚛𝚞𝚙𝚘", rowId: `${usedPrefix}unbanchat`},
     {title: "│🎟️│𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙳𝙴 𝙽𝚄𝙼𝙴𝚁𝙾𝚂 𝚅𝙸𝚁𝚃𝚄𝙰𝙻𝙴𝚂", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚒𝚗𝚜𝚝𝚊𝚕𝚊𝚌𝚒ó 𝚙𝚊𝚛𝚊 𝚌𝚛𝚎𝚊𝚛 𝚗ú𝚖𝚎𝚛𝚘𝚜 𝚟𝚒𝚛𝚝𝚞𝚊𝚕𝚎𝚜", rowId: `${usedPrefix}creatunumero`},
     {title: "│🧰│𝙶𝙸𝚃𝙷𝚄𝙱", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚒𝚗𝚜𝚝𝚊𝚕𝚊𝚌𝚒ó𝚗 𝚍𝚎𝚕 𝚋𝚘𝚝 𝚎𝚗 𝚃𝙴𝚁𝙼𝚄𝚇", rowId: `${usedPrefix}instalarbot`},
-    {title: "│🧰│𝙶𝙸𝚃𝙷𝚄𝙱2", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚒𝚗𝚜𝚝𝚊𝚕𝚊𝚌𝚒ó𝚗 𝚍𝚎𝚕 𝚋𝚘𝚝 𝚎𝚗 𝙿𝙰𝙶𝙸𝙽𝙰 𝙳𝙴 𝙱𝙾𝚇𝙼𝚄𝙽𝙴𝚆𝙾𝚁𝙻𝙳", rowId: `${usedPrefix}instalarbot2`},
-    {title: "│🧰│𝙶𝙸𝚃𝙷𝚄𝙱3", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚒𝚗𝚜𝚝𝚊𝚕𝚊𝚌𝚒ó𝚗 𝚍𝚎𝚕 𝚋𝚘𝚝 𝚎𝚗 𝙿𝙰𝙶𝙸𝙽𝙰 𝙳𝙴 𝙰𝙲𝙸𝙳𝙸𝙲𝙽𝙾𝙳𝙴𝚂", rowId: `${usedPrefix}instalarbot3`},
-    {title: "│🧰│𝙶𝙸𝚃𝙷𝚄𝙱4", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚒𝚗𝚜𝚝𝚊𝚕𝚊𝚌𝚒ó𝚗 𝚍𝚎𝚕 𝚋𝚘𝚝 𝚎𝚗 𝚃𝙴𝚁𝙼𝚄𝚇 𝚌𝚘𝚗 𝚕𝚊 𝚜𝚘𝚕𝚞𝚌𝚒𝚘𝚗 𝚍𝚎 𝚕𝚘𝚜 𝚋𝚘𝚝𝚘𝚗𝚎𝚜", rowId: `${usedPrefix}instalarbot4`},
+    {title: "│🧰│𝙶𝙸𝚃𝙷𝚄𝙱2", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚒𝚗𝚜𝚝𝚊𝚕𝚊𝚌𝚒ó𝚗 𝚍𝚎𝚕 𝚋𝚘𝚝 𝚎𝚗 𝙿𝙰𝙶𝙸𝙽𝙰 𝙳𝙴 𝙱𝙾𝚇𝙼𝚄𝙽𝙴𝚆𝙾𝚁𝙻𝙳", rowId: `${usedPrefix}Boxmine`},
+    {title: "│🧰│𝙶𝙸𝚃𝙷𝚄𝙱3", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚒𝚗𝚜𝚝𝚊𝚕𝚊𝚌𝚒ó𝚗 𝚍𝚎𝚕 𝚋𝚘𝚝 𝚎𝚗 𝙿𝙰𝙶𝙸𝙽𝙰 𝙳𝙴 𝙰𝙲𝙸𝙳𝙸𝙲𝙽𝙾𝙳𝙴𝚂", rowId: `${usedPrefix}acidicnodes`},
+    {title: "│🧰│𝙶𝙸𝚃𝙷𝚄𝙱4", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚒𝚗𝚜𝚝𝚊𝚕𝚊𝚌𝚒ó𝚗 𝚍𝚎𝚕 𝚋𝚘𝚝 𝚎𝚗 𝚃𝙴𝚁𝙼𝚄𝚇 𝚌𝚘𝚗 𝚕𝚊 𝚜𝚘𝚕𝚞𝚌𝚒𝚘𝚗 𝚍𝚎 𝚕𝚘𝚜 𝚋𝚘𝚝𝚘𝚗𝚎𝚜", rowId: `${usedPrefix}termux`},
     {title: "│🏧│𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝚃𝙴 𝙰 𝙼𝙸 𝙱𝙰𝚂𝙴𝙱𝙳𝙰𝚃𝙾𝚂", description: "𝚃𝚎 𝚒𝚗𝚟𝚒𝚝𝚘 𝚊 𝚛𝚎𝚐𝚒𝚜𝚝𝚛𝚊𝚛𝚝𝚎 𝚙𝚊𝚛𝚊 𝚚𝚞𝚎 𝚎𝚜𝚝é𝚜 𝚎𝚗 𝚖𝚒 𝚋𝚊𝚜𝚎 𝚍𝚎 𝚍𝚊𝚝𝚘𝚜 𝚍𝚎𝚕 𝚋𝚘𝚝 𝚛𝚎𝚐𝚒𝚜𝚝𝚎𝚛", rowId: `${usedPrefix}register`},
     {title: "│🎴│𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙻𝙾𝚂 𝚂𝚃𝙸𝙲𝙺𝙴𝚁𝚂", description: "𝙴𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚎𝚜 𝚙𝚊𝚛𝚊 𝚊𝚍𝚖𝚒𝚗𝚒𝚜𝚝𝚛𝚊𝚍𝚘𝚛𝚎𝚜 𝚍𝚎𝚕 𝚐𝚛𝚞𝚙𝚘 𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝚃𝙾𝙳𝙰𝚂 𝙻𝙰𝚂 𝙸𝙼𝙰𝙶𝙴𝙽𝙴𝚂, 𝚅𝙸𝙳𝙴𝙾𝚂 𝙾 𝙴𝙽𝙻𝙰𝙲𝙴𝚂 𝙴𝙽𝚅𝙸𝙰𝙳𝙾𝚂 𝚂𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝙽 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁", rowId: `${usedPrefix}enable autosticker`},
     {title: "│🌹│𝙿𝙴𝚁𝙵𝙸𝙻", description: "𝚎𝚜𝚝𝚊 𝚘𝚙𝚌𝚒ó𝚗 𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚝𝚞 𝚗𝚒𝚟𝚎𝚕 𝚢 𝚝𝚞 𝚋𝚊𝚜𝚎 𝚍𝚎 𝚍𝚊𝚝𝚘𝚜 𝚈 𝚝𝚞 𝚙𝚎𝚛𝚏𝚒𝚕", rowId: `${usedPrefix}perfil`},
@@ -75,6 +75,8 @@ rows: [
     {title: "│📴│𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 ", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚊𝚙𝚊𝚐𝚊𝚛 𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜", rowId: `${usedPrefix}enable simi`},
     {title: "│🏧│𝙻𝙰 𝚅𝙴𝙻𝙾𝙲𝙸𝙳𝙰𝙳 𝙳𝙴𝙻 𝙱𝙾𝚃", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚟𝚎𝚕𝚘𝚌𝚒𝚍𝚊𝚍 𝚍𝚎𝚕 𝚋𝚘𝚝", rowId: `${usedPrefix}ping`},
     {title: "│🎧│𝙰𝚄𝙳𝙸𝙾𝚂", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚊𝚞𝚍𝚒𝚘𝚜", rowId: `${usedPrefix}audiosdelbot`},
+    {title: "│🎢│𝚂𝙴𝚁𝚅𝙸𝙳𝙾𝚁 𝙳𝙴 𝙼𝙸𝙽𝙴𝙲𝚁𝙰𝙵𝚃", description: "𝚎𝚜𝚝𝚊 𝚘𝚙𝚌𝚒ó𝚗 𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚎𝚕 𝚕𝚒𝚗𝚔 𝚍𝚎𝚕 𝚜𝚎𝚛𝚟𝚒𝚍𝚘𝚛", rowId: `${usedPrefix}servidor`},
+    {title: "│📚│𝙿Á𝙶𝙸𝙽𝙰 𝙳𝙴 𝙶𝙸𝚃𝙷𝚄𝙱 ", description: "𝚎𝚜𝚝𝚊 𝚘𝚙𝚌𝚒ó𝚗 𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚙á𝚐𝚒𝚗𝚊 𝚍𝚎 𝚐𝚒𝚝𝚑𝚞𝚋", rowId: `${usedPrefix}sc`},
     {title: "│🌎 | 𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙴𝙽 𝙼𝙾𝙳𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾", description: "𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚅𝚄𝙴𝙻𝚅𝙴 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 𝚈/𝙾 𝙿𝚁𝙸𝚅𝙰𝙳𝙾", rowId: `${usedPrefix}enable public`},
     {title: "│🎑 | 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙴𝙻 𝙼𝙾𝙳𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 ", description: "𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚅𝚄𝙴𝙻𝚅𝙴 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 𝚈/𝙾 𝙿𝚁𝙸𝚅𝙰𝙳𝙾", rowId: `${usedPrefix}disable public`}, 
     {title: "│🧼│𝙷𝙴𝙽𝚃𝙰𝙸", description: "𝙼𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚖𝚎𝚗𝚞 +18", rowId: `${usedPrefix}tevaacaerlapolicía`},
@@ -87,13 +89,13 @@ rows: [
    {title: "│🏛️│𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙶𝙲𝙾𝙽𝙻𝚈", description: "𝙴𝙻 𝙱𝙾𝚃 𝚂𝙾𝙻𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁𝙰 𝙰 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚂𝙸 𝙴𝚂 𝚄𝙽 𝙲𝙷𝙰𝚃 𝙿𝚁𝙸𝚅𝙰𝙳𝙾", rowId: `${usedPrefix}disable gconly`}, 
    {title: "│💌│𝙳𝙾𝙽𝙰", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚒𝚗𝚏𝚘𝚛𝚖𝚊𝚌𝚒ó𝚗 𝚍𝚎 𝚍𝚘𝚗𝚊𝚛", rowId: `${usedPrefix}idonar`},
    {title: "│📌│𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚂𝙸 𝙱𝙾𝚃", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚎𝚕 𝚖𝚎𝚗𝚞 𝚍𝚎 𝚒𝚗𝚏𝚘𝚛𝚖𝚊𝚌𝚒ó𝚗", rowId: `${usedPrefix}informaciónbot`},
-   {title: "│👩‍💻️│𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙿𝚁𝙸𝚅𝙰𝙳𝙾", description: "𝚎𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚙𝚞𝚎𝚍𝚎 𝚜𝚎𝚛 𝚞𝚜𝚊𝚍𝚘 𝚙𝚘𝚛 𝚎𝚕 𝚐𝚛𝚞𝚙𝚘  𝚙𝚛𝚘𝚙𝚒𝚎𝚝𝚊𝚛𝚒@ 𝚍𝚎𝚕 𝚋𝚘𝚝", rowId: `${usedPrefix}enable antiprivado`},
-   {title: "│👥️│𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙿𝚁𝙸𝚅𝙰𝙳𝙾", description: "𝚎𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚙𝚞𝚎𝚍𝚎 𝚜𝚎𝚛 𝚞𝚜𝚊𝚍𝚘 𝚙𝚘𝚛 𝚎𝚕 𝚐𝚛𝚞𝚙𝚘  𝚙𝚛𝚘𝚙𝚒𝚎𝚝𝚊𝚛𝚒@ 𝚍𝚎𝚕 𝚋𝚘𝚝", rowId: `${usedPrefix}disable antiprivado`},
+   {title: "│👩‍💻️│𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚗𝚝𝚒𝚙𝚛𝚒𝚟𝚊𝚍𝚘", description: "𝚎𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚙𝚞𝚎𝚍𝚎 𝚜𝚎𝚛 𝚞𝚜𝚊𝚍𝚘 𝚙𝚘𝚛 𝚎𝚕 𝚐𝚛𝚞𝚙𝚘  𝚙𝚛𝚘𝚙𝚒𝚎𝚝𝚊𝚛𝚒@ 𝚍𝚎𝚕 𝚋𝚘𝚝", rowId: `${usedPrefix}enable antiprivado`},
+   {title: "│👥️│𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚗𝚝𝚒𝚙𝚛𝚒𝚟𝚊𝚍𝚘", description: "𝚎𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚙𝚞𝚎𝚍𝚎 𝚜𝚎𝚛 𝚞𝚜𝚊𝚍𝚘 𝚙𝚘𝚛 𝚎𝚕 𝚐𝚛𝚞𝚙𝚘  𝚙𝚛𝚘𝚙𝚒𝚎𝚝𝚊𝚛𝚒@ 𝚍𝚎𝚕 𝚋𝚘𝚝", rowId: `${usedPrefix}disable antiprivado`},
    {title: "│🤬│𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝙸𝚃𝙸𝙾𝚇𝙸𝙲𝙾𝚂", description: "𝙴𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚎𝚜 𝚙𝚊𝚛𝚊 𝚊𝚍𝚖𝚒𝚗𝚒𝚜𝚝𝚛𝚊𝚍𝚘𝚛𝚎𝚜 𝚍𝚎𝚕 𝚐𝚛𝚞𝚙𝚘 ", rowId: `${usedPrefix}enable antitoxic`},
    {title: "│😡│𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝙸𝚃𝙸𝙾𝚇𝙸𝙲𝙾𝚂", description: "𝙴𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚎𝚜 𝚙𝚊𝚛𝚊 𝚊𝚍𝚖𝚒𝚗𝚒𝚜𝚝𝚛𝚊𝚍𝚘𝚛𝚎𝚜 𝚍𝚎𝚕 𝚐𝚛𝚞𝚙𝚘", rowId: `${usedPrefix}disable antitoxic`},
   {title: "│☣️│𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝚃𝚁𝙰𝙱𝙰𝚂", description: "𝙴𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚎𝚜 𝚙𝚊𝚛𝚊 𝚊𝚍𝚖𝚒𝚗𝚒𝚜𝚝𝚛𝚊𝚍𝚘𝚛𝚎𝚜 𝚍𝚎𝚕 𝚐𝚛𝚞𝚙𝚘 ", rowId: `${usedPrefix}enable antitraba`},
   {title: "│☢️│𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝚃𝚁𝙰𝙱𝙰𝚂", description: "𝙴𝚜𝚝𝚊 𝚘𝚙𝚌𝚒𝚘𝚗 𝚜𝚘𝚕𝚊𝚖𝚎𝚗𝚝𝚎 𝚎𝚜 𝚙𝚊𝚛𝚊 𝚊𝚍𝚖𝚒𝚗𝚒𝚜𝚝𝚛𝚊𝚍𝚘𝚛𝚎𝚜 𝚍𝚎𝚕 𝚐𝚛𝚞𝚙𝚘", rowId: `${usedPrefix}disable antitraba`},
-            
+                        
 ]}, ] 
 let name = await conn.getName(m.sender)
 //let name = conn.getName(m.sender)
