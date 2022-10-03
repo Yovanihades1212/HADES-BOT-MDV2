040a8c5ebff7b0cff22fc76461c3d32463a89d39
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-10-03 00:00:56 -0500

    💖𝚃𝙷𝙴 𝙷𝙰𝙳𝙴𝚂 𝙱𝙾𝚃 𝙼𝙳 💬

---
+++ Hades/es.js
@@ -0,0 +1,15 @@
+
+import fetch from 'node-fetch'
+import { sticker } from '../lib/sticker.js'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `*Ejemplo de uso ${usedPrefix + command} https://getstickerpack.com/stickers/flork-memes-4-1*`
+let url = text
+let res = await fetch(`https://api.zacros.my.id/downloader/stickerpack?link=${url}`)
+let json = await res.json()
+for (let data of (json.result || json)) {
+const stikers = await sticker(false, data, global.packname, global.author)
+conn.sendFile(m.chat, stikers, 'sticker.webp', '', m, { asSticker: true })
+
+}}
+handler.command = /^stickerly|stickerpack\$/i
+export default handler
