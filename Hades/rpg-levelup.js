8762b5fe4c18ad8a0b890386b58693ca6eefea49
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-10-03 00:12:47 -0500

    💖𝚃𝙷𝙴 𝙷𝙰𝙳𝙴𝚂 𝙱𝙾𝚃 𝙼𝙳 💬

---
+++ Hades/rpg-levelup.js
@@ -0,0 +1,46 @@
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { levelup } from '../lib/canvas.js'
+
+let handler = async (m, { conn }) => {
+let { role } = global.db.data.users[m.sender]
+let name = conn.getName(m.sender)
+let user = global.db.data.users[m.sender]
+    if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+        let { min, xp, max } = xpRange(user.level, global.multiplier)
+        throw `
+┌──「𓈃ּ ៹🍏𝑯𝒂𝒅𝒆𝒔𐂂!s⁹⁷☻‹𝟹♞ᵇᵒᵗ⁻ᴹᴰ」─
+├❏๖ۣۜۜ͜͡𝐇𝐨𝐥𝐚ঔৣֳ᷌᷈͜͡ 💖✨
+├❏NIVEL
+├❏▢ Nombre : *${name}*
+├❏▢ Nivel : *${user.level}*
+├❏▢ XP : *${user.exp - min}/${xp}*
+├❏Te falta *${max - user.exp}* de *XP* para subir de nivel
+└────ׂ─ׂ─ׂ─ׂ───`.trim()
+    }
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+    if (before !== user.level) {
+        let teks = `Bien hecho! ${conn.getName(m.sender)} Nivel: ${user.level}`
+        let str = `
+┌──「𓈃ּ ៹🍏𝑯𝒂𝒅𝒆𝒔𐂂!s⁹⁷☻‹𝟹♞ᵇᵒᵗ⁻ᴹᴰ」─
+├❏๖ۣۜۜ͜͡𝐇𝐨𝐥𝐚ঔৣֳ᷌᷈͜͡ 💖✨
+├❏๖LEVEL UP*
+├❏๖Nivel anterior : *${before}*
+├❏๖Nivel actual : *${user.level}*
+├❏Cuanto más interactúes con los bots, mayor será tu nivel_*
+└────ׂ─ׂ─ׂ─ׂ───`.trim()
+        try {
+            const img = await levelup(teks, user.level)
+            conn.sendFile(m.chat, img, 'https://i.imgur.com/AYfkKZB.jpeg', str, m)
+        } catch (e) {
+            m.reply(str)
+        }
+    }
+}
+
+handler.help = ['levelup']
+handler.tags = ['xp']
+
+handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
+
+export default handler
