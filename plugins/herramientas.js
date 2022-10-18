25f826ab9945d23fc2ed50fc7fb014087c013e2e
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-10-18 01:58:56 -0500

    💖𝚃𝙷𝙴 𝙷𝙰𝙳𝙴𝚂 𝙱𝙾𝚃 𝙼𝙳💬

---
+++ plugins/herramientas.js
@@ -0,0 +1,71 @@
+import fs, { promises } from 'fs'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) => {
+try {
+let vn = './media/menu.mp3'
+let pp = './src/Me.jpg'
+let img = await(await fetch('https://i.imgur.com/lkch77g.jpg')).buffer()
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850)   
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let str = `
+┌「𓈃ּ ៹🍏𝑯𝒂𝒅𝒆𝒔𐂂!s⁹⁷☻‹𝟹♞ᵇᵒᵗ⁻ᴹᴰ」
+├❏ ๖ۣۜۜ͜͡𝐇𝐨𝐥𝐚ঔৣֳ᷌᷈͜͡ ${taguser}💖✨ 
+├❏ 📅 𝙵𝙴𝙲𝙷𝙰: ${week}, ${date}
+├❏ 🔋𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: ${uptime}
+├❏ 📊 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂: ${rtotalreg}
+└────ׂ─ׂ─ׂ─ׂ───
+┌──「🛠𝐓𝐎𝐎𝐋𝐒 𝐌𝐄𝐍𝐔」
+├❏🔧${usedPrefix}afk <motivo
+├❏🛠️${usedPrefix}acortar <enlace / link / url
+├❏🧰${usedPrefix}calc <operacion math
+├❏🪛${usedPrefix}del <respondre a mensaje del Bot
+├❏⚙️${usedPrefix}qrcode <texto
+├❏🔩${usedPrefix}readmore <texto1| texto2>
+├❏⛓️${usedPrefix}spamwa <numero|texto|cantidad
+├❏🧰${usedPrefix}styletext <texto
+├❏🛠️${usedPrefix}traducir <texto
+└────ׂ─ׂ─ׂ─ׂ───
+`.trim()
+let buttons = [
+{ buttonId: '/menu', buttonText: { displayText: '📚𝐌𝐄𝐍𝐔🍀' }, type: 1 },
+{ buttonId: '/estado', buttonText: { displayText: '📔𝐄𝐒𝐓𝐀𝐃𝐎🌱' }, type: 1 }]
+let buttonMessage = {
+image: imagen3 ,
+caption: str.trim(),
+mentions: [m.sender],
+footer: `*${wm}*`,
+buttons: buttons,
+headerType: 4,
+contextInfo: {
+mentionedJid: [m.sender],
+externalAdReply: {
+showAdAttribution: true,
+mediaType: 'VIDEO',
+mediaUrl: null,
+title: '🍀𝐓𝐇𝐄-𝐇𝐀𝐃𝐄𝐒-𝐁𝐎𝐓-𝐌𝐃✨',
+body: null,
+thumbnail: img,
+sourceUrl: `https://youtu.be/HoxZuQokeMM`
+}}}
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+//await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+} catch {
+conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
+}}
+handler.command = /^(herramientas|herramientas\?)$/i
+handler.exp = 50
+handler.fail = null
+export default handler
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
---
+++ plugins/info-reclutamiento.js
@@ -0,0 +1,80 @@
+let handler = async (m, { conn, usedPrefix }) => {
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]    
+let texto1 = `
+┌「𓈃ּ ៹🍏𝑯𝒂𝒅𝒆𝒔𐂂!s⁹⁷☻‹𝟹♞ᵇᵒᵗ⁻ᴹᴰ」
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫяєνєℓα¢ιÓи➣⃘⃕͜⃟⃤꙰.
+├❏ hola bien Bienvenid@s 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫяєνєℓα¢ιÓи➣⃘⃕͜⃟⃤꙰.
+├❏──ׂ─ׂ─ׂ─ׂ───
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ ⇶Presentarse
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ ⇶con foto 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ ⇶nombre 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ ⇶edad 
+└────ׂ─ׂ─ׂ─ׂ───
+┌「☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈Ó𝐍☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ 」
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Lo que se hace en esta legion 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Somo un Legion los mejores
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Atacamos grupos xxx
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Mandar a soporte 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Hacker teléfonos
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Robar número de WhatsApp
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Hacker Facebook
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Hacer números virtuales
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Hacer virus
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Espira las personas 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Hacer su propio WhatsApp 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Mandar WhatsApp inmunes
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Troyanos
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Puedes usar el bot la legión 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Recuperar contraseñas 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Atacar legiones hacen cosa por diversión 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Joder personas cp
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Envira virus en laboratorio 
+└────ׂ─ׂ─ׂ─ׂ───
+┌「☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫяєνєℓα¢ιÓи➣⃘⃕͜⃟⃤꙰.」
+├❏──ׂ─ׂ─ׂ─ׂ───
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫSi quieres a prender 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫponte las insignias 
+└────ׂ─ׂ─ׂ─ׂ───
+┌「☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫяєνєℓα¢ιÓи➣⃘⃕͜⃟⃤꙰.」
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫINSIGNIAS☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ  
+├❏──ׂ─ׂ─ׂ─ׂ───
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ  ➣⃘⃕͜⃟⃤꙰.
+└────ׂ─ׂ─ׂ─ׂ───
+┌「☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫяєνєℓα¢ιÓи➣⃘⃕͜⃟⃤꙰.」
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Gracias por su atención 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Somos una legión
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ Todo esto lo hacemos con el fin de hacer le bien 
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ No por beneficios propio
+├❏ ☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫ яєνєℓα¢ιÓи➣⃘⃕͜⃟⃤꙰.
+└────ׂ─ׂ─ׂ─ׂ───
+┌「☠︎︎⃟ࣩࣧ⸸₂₀ᷳ₂ᷫяєνєℓα¢ιÓи➣⃘⃕͜⃟⃤꙰.」
+https://chat.whatsapp.com/LGvb0X9GJM0LXE6yvWIE6x
+└────ׂ─ׂ─ׂ─ׂ───`
+let buttonMessage= {
+'document': { url: `https://youtu.be/4eA5nDxPjOA` },
+'mimetype': `application/${document}`,
+'fileName': `🍀𝐓𝐇𝐄-𝐇𝐀𝐃𝐄𝐒-𝐁𝐎𝐓-𝐌𝐃✨`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': 'https://github.com/Yovanihades1212/HADES-BOT-MDV2.git',
+'mediaType': 2,
+'previewType': 'pdf',
+'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
+'body': wm,
+'thumbnail': imagen1,
+'sourceUrl': 'https://youtu.be/4eA5nDxPjOA' }},
+'caption': texto1,
+'footer': wm,
+'buttons':[
+{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙼𝙴𝙽𝚄'}, type: 1}, 
+{buttonId: `${usedPrefix}donar`, buttonText: {displayText: '𝙳𝙾𝙽𝙰𝚁'}, type: 1}],
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
+handler.command = ['reclutamiento','reclutamiento']
+export default handler
