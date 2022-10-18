12f76901c3606dff01a5e48af0cabfccc6b48e57
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-10-18 02:02:38 -0500

    💖𝚃𝙷𝙴 𝙷𝙰𝙳𝙴𝚂 𝙱𝙾𝚃 𝙼𝙳💬

---
+++ plugins/nv-ñi.js
@@ -0,0 +1,15 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = './media/ñi.mp3'
+conn.sendFile(m.chat, vn, 'ñi.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /ñi|Ñi/
+handler.command = new RegExp
+export default handler
+
